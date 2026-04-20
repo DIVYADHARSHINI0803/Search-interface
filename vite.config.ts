1a71de7b86638188888e7a282577769c49505756
Divyadharshini <divyathangam08@gmail.com>
2026-04-20 17:54:54 +0530

    changes in json file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-/* export default defineConfig({
+export default defineConfig({
   plugins: [react()],
-  base: '/Search-interface/' 
-}) */
-
-  export default defineConfig({
-  base: "/Search-interface/",
+  base: "/",
 });
-
